19b15e3dbc49fdc163aa4db70fd8504a3c6fd0db
elango <mjelango240@gmail.com>
2024-11-12 22:33:11 +0530

    first commit

---
+++ src/test/java/Automation/ExtentReports/ExtentReportDemo.java
@@ -0,0 +1,48 @@
+package Automation.ExtentReports;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ExtentReportDemo {
+	ExtentReports extent;
+	
+	@BeforeTest
+	public void ReportSetup() {
+		
+		String path=System.getProperty("user.dir")+"//Reports//index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Web Automation Results");
+		reporter.config().setDocumentTitle("Test Results");
+		
+		extent=new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Elango");
+		
+	}
+	
+	@Test
+	public void initialDemo() {
+		ExtentTest test=extent.createTest("Demo");
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/client");
+		String title=driver.getTitle();
+		System.out.println(title);
+		driver.close();
+		test.fail("Test case failed");
+		extent.flush();
+	}
+
+}

26067c4b99374c526093f859222f7c7772427c92
elango <mjelango240@gmail.com>
2024-11-16 20:27:53 +0530

    new branch

--- src/main/java/Automation/ExtentReports/App.java
+++ src/main/java/Automation/ExtentReports/App.java
@@ -6,5 +6,10 @@ package Automation.ExtentReports;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World5");
+    }
+    public static void asv() {
+        System.out.println("Hello World!");
+        System.out.println("Hello World5");
     }
 }
